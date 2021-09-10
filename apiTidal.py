9c9a2db78a42a4ddc146c8b6983c84b740cb1a6b
dowitsch <fexu@whatwedo.ch>
2021-09-10 21:41:45 +0200

    wip

--- apiTidal.py
+++ apiTidal.py
@@ -1,17 +1,24 @@
 from flask import Flask, json, session, request
 import tidalapi
+from IPython import embed
+import mariadb
+import sys
+
+
 
 api = Flask(__name__)
 api.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @api.route('/authenticate', methods=['GET'])
-def get_authenticate():
+def authenticate():
 	conn = tidalapi.Session()
 	conn.login_oauth_simple()
-	session['tidal_key'] = conn.access_token
-	session['tidal_session_id'] = conn.session_id
-	session['tidal_token_type'] = conn.token_type
-	return json.dumps(session['tidal_session_id'])
+	data = {'tidal_session_id': conn.session_id, 'tidal_key': conn.access_token, 'tidal_token_type': conn.token_type }
+	db = connect()
+	cursor = db.cursor()
+	cursor.execute("INSERT INTO tidalApi (session,tidal_key,type) VALUES (?, ?, ?)", (conn.session_id, conn.access_token, conn.token_type))
+	db.commit()
+	return json.dumps(data)
 
 
 @api.route('/tracks', methods=['GET'])
@@ -19,17 +26,39 @@ def get_tracks():
 	conn = get_client()
 	name = request.args.get('name')
 	result = conn.search('track', name)
-	tracks = {}
+	tracks = []
 	index = 0
 	for track in result.tracks:
-		tracks[track.name] = {'duration': track.duration, 'artist': track.artist.name, 'album': track.album.name, 'available': track.available}
+		tracks.append({'id': track.id, 'name': track.name, 'duration': track.duration, 'artist': track.artist.name, 'album': track.album.name, 'available': track.available})
 	return json.dumps(tracks)
 
 
+@api.route('/addTrack', methods=['GET'])
+def add_track_to_favorite():
+	conn = get_client()
+	name = request.args.get('track')
+	playlist = conn.user.favorites.add_track(name)
+	return json.dumps(['works'])
+
+
+
 def get_client():
+	db = connect()
+	cursor = db.cursor()
+	cursor.execute("SELECT * FROM tidalApi ORDER BY ID DESC LIMIT 1")
+	data = cursor.fetchone()
 	conn = tidalapi.Session()
-	conn.load_oauth_session(session['tidal_session_id'], session['tidal_token_type'], session['tidal_key'])
+	conn.load_oauth_session(data[1], data[3], data[2])
 	return conn
 
+def  connect():
+	return mariadb.connect(
+		user="root",
+		password="root",
+		host="127.0.0.1",
+		port=3306,
+		database="rauschpflicht"
+	)
+
 if __name__ == '__main__':
-    api.run()
+	api.run()
